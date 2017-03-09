1eb4e780eb06ca396058fc09a70b2ad1a205f587
Ben Anderl <benboozled@gmail.com>
2017-03-08 18:57:41 -0700

    registration updates

--- cs5530/Reservation.java
+++ cs5530/Reservation.java
@@ -1,25 +1,84 @@
-//import java.util.Date;
-//
-///**
-// * Created by ben on 3/6/17.
-// */
-//public class Reservation {
-//	Reservation(){};
-//	String m_login;
-//	int m_hid;
-//	int m_pid;
-//	Date m_in = new Date();
-//	Date m_out = new Date();
-//	float m_cost;
-//	String createReservation(cs5530.User user)
-//	{
-//		System.out.println("Please enter the desired hid");
-//		while ((this.m_hid = in.readLine()) == null && this.m_hid.length() == 0 && c >= 0 && c <= 1) ;
-//		System.out.println("Please enter the desired check-in date");
-//		while ((this.m_hid = in.readLine()) == null && this.m_hid.length() == 0 && c >= 0 && c <= 1) ;
-//		System.out.println("Please enter the desired check-out");
-//		while ((this.m_hid = in.readLine()) == null && this.m_hid.length() == 0 && c >= 0 && c <= 1) ;
-//
-//		return "whatever";
-//	}
-//}
+package cs5530;
+import java.sql.*;
+import java.util.Date;
+import java.io.*;
+import java.lang.*;
+
+/**
+ * Created by ben on 3/6/17.
+ */
+public class Reservation {
+	Reservation(){};
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	String m_login;
+	int m_hid;
+	int m_pid;
+	Date m_in = new Date();
+	Date m_out = new Date();
+	float m_cost;
+
+	boolean createReservation(String startDate, String endDate, int hid, Statement stmt) {
+		String sql = "Select * from Reserve R, Period P where R.hid = P.pid and R.pid = " + hid + " and P.to >= DATE_FORMAT('" + startDate + "', '%Y-%c-%d' )AND P.from  <= DATE_FORMAT('" + endDate + "', '%Y-%c-%d') ";
+		String output = "";
+		ResultSet rs = null;
+		int c = 0;
+
+		while (c == 1)
+		{
+			try {
+				rs = stmt.executeQuery(sql);
+				while (rs.next()) {
+					output += rs.getString("pid");
+				}
+
+				rs.close();
+			} catch (Exception e) {
+				System.err.println(e);
+				System.out.println("cannot execute the query");
+			} finally {
+				try {
+					if (rs != null && !rs.isClosed())
+						rs.close();
+				} catch (Exception e) {
+					System.out.println("cannot close resultset");
+				}
+			}
+			if (output != "") {
+				try {
+					c = Integer.parseInt(readInput("A reservation exists for that date. Try again?\n 1: yes\n2: no"));
+				} catch (Exception e) {
+					c = -1;
+					continue;
+				}
+			} else {
+				try {
+					c = Integer.parseInt(readInput("Dates availiable! Do you wish to proceed with reservation creation? " +
+							"The following reservation will be made:\n" +
+							"From " + startDate + " to " + endDate + " for housing number " + hid + "\n 1: yes\n2: no"));
+				} catch (Exception e) {
+					c = -1;
+					continue;
+				}
+
+			}
+
+			return false;
+		}
+	}
+
+	public static String readInput(String message)
+	{
+		System.out.println(message);
+
+		String temp = "";
+		try
+		{
+			while ((temp = in.readLine()) == null && temp.length() == 0) ;
+		}
+		catch(Exception e)
+		{
+			System.out.println("Invalid Input");
+		}
+		return temp;
+	}
+}
